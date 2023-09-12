e9cf296491599a86b26e427bbb77072d6c390757
Rick Hallett <rick.hallett@brandwatch.com>
2023-09-12 23:19:03 +0100

    feat(core): test FromSchema

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import { UserType, User } from "./typebox/User";
+import { todo } from "./schemas/todo";
 
 interface IQuerystring {
   username: string;
@@ -93,7 +94,40 @@ server.route<{
     const customerHeader = request.headers['h-Custom']
     reply.status(200).send({username});
   }
-})
+});
+
+import { FromSchema } from "json-schema-to-ts";
+
+server.post<{ Body: FromSchema<typeof todo> }>(
+  '/todo',
+  {
+    schema: {
+      body: todo,
+      response: {
+        201: {
+          type: 'string',
+        },
+      },
+    }
+  },
+  async (request, reply): Promise<void> => {
+
+    /*
+    request.body has type
+    {
+      [x: string]: unknown;
+      description?: string;
+      done?: boolean;
+      name: string;
+    }
+    */
+
+    request.body.name // will not throw type error
+    request.body.notthere // will throw type error
+
+    reply.status(201).send(JSON.stringify({requestBody: request.body}));
+  },
+);
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
---
+++ src/schemas/todo.ts
@@ -0,0 +1,9 @@
+export const todo = {
+    type: 'object',
+    properties: {
+      name: { type: 'string' },
+      description: { type: 'string' },
+      done: { type: 'boolean' },
+    },
+    required: ['name'],
+  } as const; // don't forget to use const !
