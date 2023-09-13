4eea8b02bbede5ad3cafafd17ab22def4505605b
Rick Hallett <rick.hallett@brandwatch.com>
2023-09-13 22:25:09 +0100

    feat(core): setup swagger and swagger-ui with mocks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Fastify, { FastifyInstance } from "fastify";
+// import type * as global from "./global.d.ts";
 import { getPing } from "./routes/utils/getPing";
 import { createUser } from "./routes/auth/createUser";
 import { queryParamLogin } from "./routes/auth/queryParamLogin";
@@ -6,16 +7,23 @@ import { createTodo } from "./routes/todo/createTodo";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import autoLoad from "@fastify/autoload";
 import { join } from "path";
+import prismaPlugin from "./plugins/prisma";
+import { fastifySwaggerPlugin } from "./plugins/swagger";
+import { fastifySwaggerUIPlugin } from "./lib/spec/swagger/html";
 
 const fastify: FastifyInstance = Fastify(
   {}
 ).withTypeProvider<TypeBoxTypeProvider>();
 
-fastify.register(autoLoad, {
-  dir: join(__dirname, "plugins"),
-});
+// fastify.register(autoLoad, {
+//   dir: join(__dirname, "plugins"),
+// });
 
 fastify.register(import("@fastify/routes"));
+
+fastify.register(prismaPlugin);
+fastify.register(fastifySwaggerPlugin);
+fastify.register(fastifySwaggerUIPlugin);
 
 fastify.register(getPing);
 fastify.register(createUser);
@@ -28,7 +36,20 @@ fastify.listen({ port: 8080 }, (err, address) => {
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);
+
+  startSwagger();
+
   console.log(fastify.printRoutes({ includeHooks: true }));
   console.log(fastify.printPlugins());
   // console.log(fastify.routes);
 });
+
+async function startSwagger() {
+  try {
+    await fastify.ready();
+    fastify.swagger();
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
---
+++ src/lib/spec/swagger/html.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const fp = require("fastify-plugin");
+
+function fastifySwaggerUI(fastify, opts, next) {
+  fastify.register(require("@fastify/swagger-ui"), {
+    routePrefix: "/docs",
+    swagger: {
+      info: {
+        title: "My FirstAPP Documentation",
+        description: "My FirstApp Backend Documentation description",
+        version: "0.1.0",
+        termsOfService: "https://mywebsite.io/tos",
+        contact: {
+          name: "John Doe",
+          url: "https://www.johndoe.com",
+          email: "john.doe@email.com",
+        },
+      },
+      externalDocs: {
+        url: "https://www.johndoe.com/api/",
+        description: "Find more info here",
+      },
+      host: "127.0.0.1:3000",
+      basePath: "",
+      schemes: ["http", "https"],
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      tags: [
+        {
+          name: "User",
+          description: "User's API",
+        },
+      ],
+      definitions: {
+        User: {
+          type: "object",
+          required: ["id", "email"],
+          properties: {
+            id: {
+              type: "number",
+              format: "uuid",
+            },
+            firstName: {
+              type: "string",
+            },
+            lastName: {
+              type: "string",
+            },
+            email: {
+              type: "string",
+              format: "email",
+            },
+          },
+        },
+      },
+    },
+    uiConfig: {
+      docExpansion: "none", // expand/not all the documentations none|list|full
+      deepLinking: true,
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) {
+        next();
+      },
+      preHandler: function (request, reply, next) {
+        next();
+      },
+    },
+    staticCSP: false,
+    transformStaticCSP: (header) => header,
+    exposeRoute: true,
+  });
+
+  next()
+}
+
+module.exports.fastifySwaggerUIPlugin = fp(fastifySwaggerUI, {
+  fastify: "4.x",
+  name: "@fastify/swagger",
+});
