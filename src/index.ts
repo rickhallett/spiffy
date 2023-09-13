0b1c17c7eefcaab23c8ef13441054208844616df
Rick Hallett <rick.hallett@brandwatch.com>
2023-09-13 17:26:12 +0100

    feat(core): autoload for plugins

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,21 @@ import { getPing } from "./routes/utils/getPing";
 import { createUser } from "./routes/auth/createUser";
 import { queryParamLogin } from "./routes/auth/queryParamLogin";
 import { createTodo } from "./routes/todo/createTodo";
-import prismaPlugin from "./plugins/prisma";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import autoLoad from "@fastify/autoload";
+import { join } from "path";
+import fastify from "fastify";
+
+const app = fastify();
 
 const server: FastifyInstance = Fastify(
   {}
 ).withTypeProvider<TypeBoxTypeProvider>();
 
-server.register(prismaPlugin);
+server.register(autoLoad, {
+  dir: join(__dirname, "plugins"),
+});
+
 server.register(getPing);
 server.register(createUser);
 server.register(queryParamLogin);
