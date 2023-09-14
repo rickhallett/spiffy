39142b046bcbc63b409424363b877f1a800d7134
Rick Hallett <rick.hallett@brandwatch.com>
2023-09-14 19:56:40 +0100

    fix(deploy): clerk redirect url

--- src/routes/auth/register.ts
+++ src/routes/auth/register.ts
@@ -4,7 +4,7 @@ import fp from 'fastify-plugin';
 export const register: FastifyPluginCallback = fp((fastify, options, done) => {
   fastify.get('/auth/register', async (request, reply) => {
     reply.redirect(
-      'https://fresh-tetra-83.accounts.dev/sign-up?redirect_url=http://localhost:8080/home'
+      `https://fresh-tetra-83.accounts.dev/sign-up?redirect_url=${request.originalUrl}/home`
     );
   });
 
